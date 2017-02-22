25eb4bd689797b81086ef68eff33979efc5194ff
viento006 <viento006@gmail.com>
2017-02-22 13:29:39 +0300

    Updated urls.

--- utils/WebAPI.js
+++ utils/WebAPI.js
@@ -1,5 +1,5 @@
 const SERVER_HOST = 'localhost:8080';
-const COWORKIO_HOST_URL = (window.location ? window.location.protocol : 'http:') + '//' + SERVER_HOST;
+const COWORKIO_HOST_URL = window.location ? window.location.protocol  + '//' + SERVER_HOST : 'https://coworkio.herokuapp.com';
 
 const WebAPI = {
     ACCOUNT_LOGIN: COWORKIO_HOST_URL + '/auth/login',
@@ -8,10 +8,10 @@ const WebAPI = {
     FETCH_TASK :  COWORKIO_HOST_URL + '/task/{taskID}',
     FETCH_TASKS :  COWORKIO_HOST_URL + '/task/{projectID}/all',
     FETCH_PROJECTS: COWORKIO_HOST_URL + '/project/all',
-    FETCH_PROJECT: COWORKIO_HOST_URL + '/project/',
+    FETCH_PROJECT: COWORKIO_HOST_URL + '/project',
     CREATE_PROJECT: COWORKIO_HOST_URL + '/project/create',
     FETCH_PROJECTS_BY_USER: COWORKIO_HOST_URL + '/user/getProjects',
-    FETCH_USER_PROFILE: COWORKIO_HOST_URL + '/user/profile/',
+    FETCH_USER_PROFILE: COWORKIO_HOST_URL + '/user/profile',
     UPDATE_USER_PROFILE: COWORKIO_HOST_URL + '/user/update'
 };
 
